a2c19d7b24c5f71aa298ab07a1989739cbd0b9dd
dmusila <dennis.munguti@live.com>
2017-04-04 10:46:30 -0700

    Add files via upload

---
+++ DataTypeLab.py
@@ -0,0 +1,50 @@
+def data_type(datainput):
+
+
+	if datainput is None:
+		return "no value"
+
+	elif type(datainput) is str:
+		return len(datainput)
+	elif type(datainput) is bool:
+		return datainput
+	elif type(datainput) is int:
+		if datainput == 100:
+			return "equal to 100"
+		elif datainput < 100:
+			return "less than 100"
+		else:
+			return "more than 100"
+	elif type(datainput) is list:
+		if len(datainput)< 3:
+			return None
+		else:
+			return datainput[2]
+
+import unittest
+
+class CarClassTest(unittest.TestCase):
+
+
+  def test_none_type(self):
+    self.assertEqual('no value', data_type(None))
+
+  def test_array_type(self):
+    self.assertEqual(3, data_type([1, 2, 3]))
+
+  def test_small_array_type(self):
+    self.assertEqual(None, data_type([1, 2]))
+
+  def test_small_booleans_type(self):
+    self.assertEqual(True, data_type(True))
+
+  def test_small_integer_type(self):
+    self.assertEqual('less than 100', data_type(3))
+
+  def test_large_integer_type(self):
+    self.assertEqual('more than 100', data_type(200))
+
+  def test_str_type(self):
+    self.assertEqual(6, data_type('andela'))
+
+unittest.main()
