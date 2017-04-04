876e626815406c472a5ef8d97cbb30a508e5ca39
dmusila <dennis.munguti@live.com>
2017-04-04 07:53:50 -0700

    Add files via upload

---
+++ TESTBuylaptop.py
@@ -0,0 +1,10 @@
+import unittest
+import BuyLaptopOOP
+
+
+class test_buy_laptop(unittest.TestCase):
+    def testclass_inheritance(self):
+        self.assertTrue(isinstance(Gaminglaptop, Laptop))
+
+    def testclass_inheritance (self):
+        self.asserTrue(isinstance(Normallaptop, Laptop))
